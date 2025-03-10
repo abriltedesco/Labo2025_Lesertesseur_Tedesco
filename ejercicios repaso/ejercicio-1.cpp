481f640e179e3b3b1618743dc4e246bb18a1d1ab
Abril Tedesco <167026793+abriltedesco@users.noreply.github.com>
2025-03-10 12:39:19 -0300

    Add files via upload

---
+++ ejercicios repaso/ejercicio-1.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+using namespace std; 
+
+struct Fecha{
+    int dia; 
+    int mes; 
+    int anio;
+};
+
+struct Empleado{
+    string nombre;
+    string apellido;
+    Fecha fecha;
+    string sexo;
+    float salario;
+};
+
+void crearEmpleados (vector<Empleado> &empleados){
+    for(int i=0 ; i<10 ; i++){
+        cout<<"Ingreso de datos"<<endl;
+        cin>>empleados[i].nombre;
+        cin>>empleados[i].apellido;
+        cin>>empleados[i].fecha.dia;
+        cin>>empleados[i].fecha.mes;
+        cin>>empleados[i].fecha.anio;
+        cin>>empleados[i].sexo;
+        cin>>empleados[i].salario;
+    }
+}
+
+Empleado ordenarVector(vector<Empleado> &empleados){
+
+
+    for (int i = 1; i < empleados.size(); ++i) {
+        int key = empleados[i].salario;
+        int j = i - 1;
+        while (j >= 0 && empleados[j].salario > key) {
+            empleados[j + 1].salario = empleados[j].salario;
+            j = j - 1;
+        }
+        empleados[j + 1].salario = key;
+    }
+
+    int tamanio;
+    tamanio = empleados.size();
+    Empleado mayor;
+    mayor = empleados[tamanio - 1] ;
+
+    return mayor;
+}
+
+Empleado empleadoConMayorSueldo (vector<Empleado> &empleados, vector<Empleado> &eMenorSalario){
+    for(Empleado e: empleados){
+        if(e.salario <= 400000){
+            vector <Empleado> eMenorSalario;
+            eMenorSalario.push_back(e);
+            cout<< e.nombre << endl;
+            cout<< e.apellido << endl;
+            cout<< e.sexo << endl;
+            cout<< e.salario << endl; 
+        }
+        else{
+            return ordenarVector(empleados);
+        }
+    }
+}
+
+
+
+int main(){
+    Empleado empleado;
+    vector<Empleado> empleados;
+    vector<Empleado> eMenorSalario;
+    crearEmpleados(empleados);
+
+    cout << "Empleado con mayor sueldo: " ;
+    Empleado emp = empleadoConMayorSueldo(empleados, eMenorSalario);
+    cout << emp.nombre << " " <<emp.apellido << " su suedo es: " << emp.salario;
+
+    cout << "Empleados con menor sueldo: " <<endl;
+    for (Empleado e: eMenorSalario){
+        cout << e.nombre << " " << e.apellido;
+    }
+}
