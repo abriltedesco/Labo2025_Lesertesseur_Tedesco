10e802ece6fb1174518775337771a316c29fc8c2
= <=>
2025-04-09 09:33:09 -0300

    ejercicios terminado

--- Tedesseur/src/unidad1/Rectangulo.java
+++ Tedesseur/src/unidad1/Rectangulo.java
@@ -4,6 +4,11 @@ public class Rectangulo {
     private double base;
     private double altura;
 
+    public Rectangulo(){
+        this.base = 6 ;
+        this.altura = 8;
+    }
+
     public Rectangulo(double base, double altura){
         this.base = base;
         this.altura = altura;
@@ -14,6 +19,14 @@ public class Rectangulo {
     }
     public double getAltura(){
         return altura;
+    }
+
+    public void setBase(double base){
+        this.base = base;
+    }
+
+    public void setAltura(double altura){
+        this.altura = altura;
     }
 
     public double calcularArea(double altura, double base){
@@ -30,10 +43,17 @@ public class Rectangulo {
 
     public static void main(String[] args) {
         Rectangulo r1 = new Rectangulo(5,3);
-        System.out.println(r1.getBase());
-        System.out.println(r1.getAltura());
-        System.out.println(r1.calcularArea(r1.getAltura(), r1.getBase()));
-        System.out.println(r1.calcularPerimetro(r1.getAltura(), r1.getBase()));
+        Rectangulo r2 = new Rectangulo();
+
+        r2.setBase(9);
+        r2.setAltura(10);
+
+        System.out.println("R1: " + r1.getBase());
+        System.out.println("R1: " + r1.getAltura());
+        System.out.println("R2: " + r2.getBase());
+        System.out.println("R2: " +r2.getAltura());
+        System.out.println("R1: " + r1.calcularArea(r1.getAltura(), r1.getBase()));
+        System.out.println("R1: " + r1.calcularPerimetro(r1.getAltura(), r1.getBase()));
     }
 
 }
