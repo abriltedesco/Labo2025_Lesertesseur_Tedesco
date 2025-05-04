e4e2e0ef1d9b5f7b897100c616d650c2a621cd40
Eugeles <eufrales@gmail.com>
2025-05-04 14:38:13 -0300

    ejercicios para clase 5/5

---
+++ duke/choice/Clothing.java
@@ -0,0 +1,48 @@
+package duke.choice;
+
+public class Clothing {
+    private String description;
+    private double price;
+    private String size;
+    private final double minPrice = 10;
+    private final double tax = 0.2;
+
+    public Clothing (String description, double price, String size){
+        this.description = description;
+        this.price = price;
+        this.size = size;
+    } 
+
+    public String getDescription(){
+        return description;
+    }
+
+    public String setDescription(String description){
+        this.description = description;
+        return description;
+    }
+
+    public double getPrice(){
+        this.price = (this.price * tax) + this.price; 
+        return price;
+    }
+
+    public double setPrice(double price){
+        this.price = price;
+        if(this.price < minPrice){
+            this.price = minPrice;
+        }
+        return price;
+    }
+
+    public String getSize(){
+        return size;
+    }
+
+    public String setSize(String size){
+        this.size = size;
+        return size;
+    }
+
+
+}
